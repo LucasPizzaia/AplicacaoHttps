a5e99edbdaaae13ea4a82834943f5fe9ceb17c4f
LucasPizzaia <lucas.pizzaia@edu.unifil.br>
2024-08-19 19:54:39 -0300

    Arrumando código

---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'l12102004',
+    database: 'api-node',
+    define: {
+      timestamps: true,
+      underscored: true
+    }
+  }
+  
